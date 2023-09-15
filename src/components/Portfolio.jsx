826e9dce32d790659a77a16d1e757f47068f335f
Elian Rivoira <eli_rivoira@hotmail.com>
2023-09-15 18:46:33 -0300

    added rojo d-i to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,49 +5,59 @@ import prode from '../assets/portfolio/prode.png';
 import aidamSite from '../assets/portfolio/aidam-site.png';
 import aidamApp from '../assets/portfolio/aidam-app.png';
 import appointmentApp from '../assets/portfolio/appointment-app.png';
+import rojodei from '../assets/portfolio/rojodei.png';
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: tmdb,
-      demo: 'https://tmdatabase.vercel.app',
-      code: 'https://github.com/ElianRivoira/TMDB',
-    },
-    {
-      id: 2,
-      src: allio,
-      demo: '',
-      code: 'https://github.com/ElianRivoira/Al-Lio-Clothing-Store',
-    },
-    {
-      id: 3,
-      src: prode,
-      demo: '',
-      code: 'https://github.com/ElianRivoira/PRODE-Tonic3',
-    },
-    {
-      id: 4,
       src: aidamSite,
       demo: 'https://www.aidam.com.ar',
       code: 'https://github.com/ElianRivoira/aidam-site',
     },
     {
-      id: 5,
+      id: 2,
       src: aidamApp,
       demo: '',
       code: 'https://github.com/ElianRivoira/aidam',
     },
     {
-      id: 6,
+      id: 3,
       src: appointmentApp,
       demo: 'https://appointment-app-elianrivoira.vercel.app',
       code: 'https://github.com/ElianRivoira/appointmentApp',
     },
+    {
+      id: 4,
+      src: rojodei,
+      demo: 'https://rojodei.com.ar/#/',
+      code: 'https://github.com/RojoEmiliano/Rojo-D-I',
+    },
+    {
+      id: 5,
+      src: tmdb,
+      demo: 'https://tmdatabase.vercel.app',
+      code: 'https://github.com/ElianRivoira/TMDB',
+    },
+    {
+      id: 6,
+      src: allio,
+      demo: '',
+      code: 'https://github.com/ElianRivoira/Al-Lio-Clothing-Store',
+    },
+    {
+      id: 7,
+      src: prode,
+      demo: '',
+      code: 'https://github.com/ElianRivoira/PRODE-Tonic3',
+    },
   ];
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    <div
+      name='portfolio'
+      className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'
+    >
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
