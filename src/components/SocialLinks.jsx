280fed15d624ad760cc42107cc9422e8dec84a5c
Elian Rivoira <eli_rivoira@hotmail.com>
2023-09-19 19:12:30 -0300

    Fixed soma issues in mobile view

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+import useMediaQuery from '../hooks/useMediaQuery';
 
 const SocialLinks = () => {
   const links = [
@@ -12,8 +14,8 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://www.linkedin.com/in/elianrivoira/",
-      style: "rounded-tr-md",
+      href: 'https://www.linkedin.com/in/elianrivoira/',
+      style: 'rounded-tr-md',
     },
     {
       id: 2,
@@ -22,16 +24,16 @@ const SocialLinks = () => {
           GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/ElianRivoira",
+      href: 'https://github.com/ElianRivoira',
     },
     {
       id: 3,
       child: (
         <>
-          Mail <HiOutlineMail size={30} />
+          E-Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:rivoiraelian@gmail.com",
+      href: 'mailto:rivoiraelian@gmail.com',
     },
     {
       id: 4,
@@ -40,37 +42,63 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/elianRivoira.pdf",
-      style: "rounded-br-md",
+      href: '/elianRivoira.pdf',
+      style: 'rounded-br-md',
       download: true,
     },
   ];
 
   return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
-      <ul>
-        {links.map(({ id, child, href, style, download }) => (
-          <li
-            key={id}
-            className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-              " " +
-              style
-            }
-          >
-            <a
-              href={href}
-              className="flex justify-between items-center w-full text-white"
-              download={download}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {child}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {useMediaQuery(1024) ? (
+        <footer className='bg-gray-800 py-4 flex justify-center'>
+          <div className='flex w-full justify-evenly smMax:flex-wrap gap-3'>
+            {links.map(({ id, child, href, style, download }) => (
+              <div
+                key={id}
+                className={`flex justify-between items-center smMax:w-[150px] sm:w-full max-w-[160px] min-w-[95px] px-4 py-1 
+                hover:rounded-md duration-300 bg-gray-500 rounded-md ${style}`}
+              >
+                <a
+                  href={href}
+                  className='flex flex-col-reverse gap-1 justify-between items-center w-full text-white'
+                  download={download}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {child}
+                </a>
+              </div>
+            ))}
+          </div>
+        </footer>
+      ) : (
+        <div className='flex flex-col top-[35%] left-0 fixed'>
+          <ul>
+            {links.map(({ id, child, href, style, download }) => (
+              <li
+                key={id}
+                className={
+                  'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' +
+                  ' ' +
+                  style
+                }
+              >
+                <a
+                  href={href}
+                  className='flex justify-between items-center w-full text-white'
+                  download={download}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {child}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'lgMax': {'max': '1024px'},
+        'smMax': {'max': '440px'},
+      },
+    },
     fontFamily: {
       signature: ["Great Vibes"],
     },
